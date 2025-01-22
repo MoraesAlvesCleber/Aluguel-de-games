67b370e85de8ce0ab2708ec5030b2599ce28df6a
Cleber Moraes <alvesmoraesb@gmail.com>
2025-01-22 09:52:27 -0300

    primeiro commit
    implementando logica nos botões e imagem.

---
+++ js/app.js
@@ -0,0 +1,17 @@
+function alterarStatus(id,event){
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    event.preventDefault();
+     
+    if(imagem.classList.contains('dashboard__item__img--rented')){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else{
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+    
+}
